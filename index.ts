5eb0c9364bc84eb351e186d21adf62f356c557e5
adebayo-adewunmi <adebayo.adewunmi@africaprudential.com>
2022-09-11 10:52:45 +0100

    warning fix

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 import { rateLimit } from "express-rate-limit";
 
 dotenv.config();
@@ -15,23 +15,23 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-app.get('/howold', (req: Request, res: Response) => {
+app.get("/howold", (req: Request, res: Response) => {
     let queryParams :any = req.query; 
-    let regexWithSlash = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
-    let regexWithHyphen = /^([0-2][0-9]|(3)[0-1])(-)(((0)[0-9])|((1)[0-2]))(-)\d{4}$/i;
+    let regexWithSlash = /^([0-2]\d|(3)[0-1])(\/)(((0)\d)|((1)[0-2]))(\/)\d{4}$/i;
+    let regexWithHyphen = /^([0-2]\d|(3)[0-1])(-)(((0)\d)|((1)[0-2]))(-)\d{4}$/i;
 
    
     if(!queryParams.hasOwnProperty("dob") ){
       res.status(400).json({
         status: "Bad Request",
-        message: "dob parameter is required"
+        message: "dob parameter is required",
       });
     }
 
     if(!regexWithSlash.test(queryParams.dob) && !regexWithHyphen.test(queryParams.dob)){
       res.status(400).json({
         status: "Bad Request",
-        message: "Error: Date of Birth must be in any of this formats [dd/mm/yyyy, dd-mm-yyyy]"
+        message: "Error: Date of Birth must be in any of this formats [dd/mm/yyyy, dd-mm-yyyy]",
       });
     }
 
@@ -40,7 +40,7 @@ app.get('/howold', (req: Request, res: Response) => {
 
     res.status(200).json({
       status: "success",
-      message: `Hey! buddie, you are ${age} years old.`
+      message: `Hey! buddie, you are ${age} years old.`,
     });
 });
 
